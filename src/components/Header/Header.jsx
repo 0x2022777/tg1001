ccf09e453cfcf71cfab071080d4fed5c32f99693
0x2022777 <t7772021@yandex.ru>
2023-01-19 18:35:53 -0500

    df

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,14 @@ import Button from 'src/components/Button/Button';
 const Header = () => {
     const tg = window.Telegram.WebApp;
     const onClose = () => {
-        tg.close();
+        tg.close()
       }
-    return {
+    return (
         <div className={'header'}>
             <Button onClick={onClose}>Жабу1</Button>
             <span className={'username'}></span>
         </div>
-    };
+    );
 };
 
 export default Header;
