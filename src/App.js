c1519edf062ba299ddb61f436b3b602d76175bcf
0x2022777 <t7772021@yandex.ru>
2023-01-19 16:17:59 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, {  useEffect } from "react";
 import './App.css';
+const tg = window.Telegram.WebApp;
 
 function App() {
+    useEffect(() => {
+        tg.ready();
+      }, []);
+    const onClose = () => {
+      tg.close();
+    }
   return (
     <div className="App">
       <h1>Бари истеп тур!!! Тамаша!</h1>
+      <button onClick={onClose}>Жабу</button>
     </div>
   );
 }
