a6ef2c6cbf5eb354f6a4b3b8aa3a320d64e26997
0x2022777 <t7772021@yandex.ru>
2023-01-19 18:29:43 -0500

    ch01

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import Button from 'src/components/Button/Button';
 
+
 const Header = () => {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close();
+      }
     return {
         <div className={'header'}>
             <Button>Жабу1</Button>
