d7513401ca82e9b59e17a0b917ff7b6ab6d50088
0x2022777 <t7772021@yandex.ru>
2023-01-19 18:29:29 -0500

    ch3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {  useEffect } from "react";
 import './App.css';
-const tg = window.Telegram.WebApp;
+
 
 function App() {
     useEffect(() => {
         tg.ready();
       }, []);
-    const onClose = () => {
-      tg.close();
-    }
+
   return (
     <div className="App">
       <h1>Бари истеп тур!!! Тамаша!</h1>
