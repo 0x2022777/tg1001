9010e31b1b37f43a9536b69b0e68902171a7ac31
0x2022777 <t7772021@yandex.ru>
2023-01-20 17:54:29 -0500

    4

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 import './Form.css';
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
     return (
-        <div>
-            <h2>You are in Form page.</h2>
+        <div className={"form"}>
+            <h2>Өзіңізді деректеріңізді енгізіңіз.</h2>
+            <input 
+                className={'input'} type="text" placeholder={'Мемлекет'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input 
+                className={'input'} type="text" placeholder={'Көше'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+
         </div>
     );
 };
