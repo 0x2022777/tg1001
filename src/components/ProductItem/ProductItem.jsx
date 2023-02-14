7b3b73d7d86a3e615ac2de409c47f003018ac461
0x2022777 <t7772021@yandex.ru>
2023-02-14 11:53:13 -0500

    123

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductItem = () => {
+    return (
+        <div className={'product' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{}</div>
+
+        </div>
+    );
+};
+
+export default ProductItem;
